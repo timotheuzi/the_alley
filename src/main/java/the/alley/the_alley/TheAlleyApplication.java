062c41ac8c294dd30ba07d01c86afd245c2322c9
timotheuzi <timotheuzi@yahoo.com>
2023-07-18 17:00:23 -0500

    fixed dependancies

--- src/main/java/the/alley/the_alley/TheAlleyApplication.java
+++ src/main/java/the/alley/the_alley/TheAlleyApplication.java
@@ -2,8 +2,10 @@ package the.alley.the_alley;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
-@SpringBootApplication
+//@SpringBootApplication
+@EnableWebFlux
 public class TheAlleyApplication {
 
 	public static void main(String[] args) {
