4c065653303ff1709581ca92eaa281b4eaa054a6
timotheuzi <timotheuzi@yahoo.com>
2023-07-18 10:50:36 -0500

    fixed dependancies

---
+++ src/main/java/the/alley/the_alley/TheAlleyApplication.java
@@ -0,0 +1,13 @@
+package the.alley.the_alley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheAlleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheAlleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/the/alley/the_alley/TheAlleyApplicationTests.java
@@ -0,0 +1,13 @@
+package the.alley.the_alley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheAlleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
